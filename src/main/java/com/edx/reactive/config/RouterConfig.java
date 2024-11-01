c23e95609beb0cd8c687b677df0bc5065b2ed349
EldadDor <eldadno1@gmail.com>
2024-11-01 20:38:19 +0200

    Saving a few changes, still not working

--- src/main/java/com/edx/reactive/config/RouterConfig.java
+++ src/main/java/com/edx/reactive/config/RouterConfig.java
@@ -3,25 +3,33 @@ package com.edx.reactive.config;
 import com.edx.reactive.http.CookieHandlerFilterFunction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Mono;
 
-public class RouterConfig   {
+@Configuration
+public class RouterConfig {
 
     @Autowired
-    private CookieHandlerFilterFunction cookieFilter;
+    private CookieHandlerFilterFunction cookieHandlerFilter;
 
     @Bean
     public RouterFunction<ServerResponse> routerFunction() {
         return RouterFunctions.route()
-                .GET("/**", this::forwardToController)
-                .POST("/**", this::forwardToController)
-                .filter(cookieFilter)
+                .path("/vehicles", builder -> builder
+                        // Match exact paths instead of using /**
+                        .GET("", this::forwardToController)
+                        .GET("/{id}", this::forwardToController)
+                        .GET("/color/{color}", this::forwardToController)
+                        .GET("/type/{type}", this::forwardToController)
+                        .POST("", this::forwardToController))
+                .filter(cookieHandlerFilter)
                 .build();
     }
 
     private Mono<ServerResponse> forwardToController(ServerRequest request) {
-        return ServerResponse.status(HttpStatus.OK).build();
+        // Forward to the RestController
+        return ServerResponse.ok().build();
     }
 }
